4222a75de5238371accc77a2e586c790e81b43b9
백승진 <vromotion@gmail.com>
2026-03-08 01:15:33 +0900

    02_array

--- week2/basic/02_array.py
+++ week2/basic/02_array.py
@@ -44,10 +44,14 @@ def rotate_matrix_90(matrix):
     n = len(matrix)
     
     # TODO: n x n 크기의 새로운 배열을 생성하세요 (0으로 초기화)
+    rotated = [[0]*n for cnt in range(n)]
     pass
         
     # TODO: 원본 배열의 각 요소를 회전된 위치에 배치하세요
     # 힌트: (i, j) 위치의 요소는 회전 후 (j, n-1-i) 위치로 이동
+    for i in range(n):
+        for j in range(n):
+            rotated[j][n-1-i] = matrix[i][j]
     pass
     
     return rotated
