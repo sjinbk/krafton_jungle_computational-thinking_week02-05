467b89b8fb276e1b745807c8f84a4c9b81386b48
백승진 <vromotion@gmail.com>
2026-03-07 21:12:52 +0900

    01_python_dict

--- week2/basic/01_python_dict.py
+++ week2/basic/01_python_dict.py
@@ -42,12 +42,21 @@ def find_above_average_students(students):
         tuple: (평균 점수, 평균 이상 학생 이름 리스트)
     """
     # TODO: 모든 학생의 점수를 리스트로 추출하세요
+    scores = []
+    for i in range(0, len(students)):
+        score = students[i]["score"]
+        scores.append(score)
     pass
     
     # TODO: 평균 점수를 계산하세요
+    average = sum(scores)/len(scores)
     pass
     
     # TODO: 평균 이상인 학생들의 이름을 리스트로 추출하세요
+    above_average_students = []
+    for i in range(0, len(students)):
+        if students[i]["score"] >= average:
+            above_average_students.append(students[i]["name"])
     pass
     
     return average, above_average_students
