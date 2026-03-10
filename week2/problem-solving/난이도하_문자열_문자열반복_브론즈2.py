43d1984e651616f929fa00e55522ce4f7c9de4ea
백승진 <vromotion@gmail.com>
2026-03-10 20:07:25 +0900

    난이도하_문자열_문자열반복_브론즈2

--- week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
+++ week2/problem-solving/난이도하_문자열_문자열반복_브론즈2.py
@@ -1,2 +1,18 @@
 # 문자열 - 문자열 반복 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/2675
+
+T = int(input())
+
+for i in range(T):
+    S = input()
+
+    if S.find(" ") != -1:
+        R, char = S.split()
+        char_final = ""
+        char_list = list(char)
+
+        for num in range(len(char_list)):
+            char_final += str(char_list[num]) * int(R)
+        print(char_final)
+    else:
+        pass
