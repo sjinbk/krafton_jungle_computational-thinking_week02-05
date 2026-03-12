9a93021efdbe41055028c30a8899d88521bf6222
백승진 <vromotion@gmail.com>
2026-03-12 21:12:47 +0900

    02_divide_conquer

--- week3/basic/02_divide_conquer.py
+++ week3/basic/02_divide_conquer.py
@@ -36,18 +36,24 @@ def find_max_divide_conquer(arr, left, right):
         최댓값
     """
     # TODO: base case - 원소가 하나면 그 값 반환
+    if left == right:
+        return arr[left]
     pass
     
     # TODO: 중간 지점 계산
+    mid = (left + right) // 2
     pass
     
     # TODO: 왼쪽 절반의 최댓값
+    find_max_divide_conquer(arr, left, mid)
     pass
         
     # TODO: 오른쪽 절반의 최댓값
+    find_max_divide_conquer(arr, mid+1, right)
     pass
     
     # TODO: 둘 중 큰 값 반환
+    return max(find_max_divide_conquer(arr, left, mid), find_max_divide_conquer(arr, mid+1, right))
     pass
 
 # 테스트 케이스
