5fdb1279034f062841bcec86ada98396f1d923a6
백승진 <vromotion@gmail.com>
2026-03-16 23:09:45 +0900

    난이도하_스택_스택_실버4

--- week3/problem-solving/난이도하_스택_스택_실버4.py
+++ week3/problem-solving/난이도하_스택_스택_실버4.py
@@ -1,2 +1,32 @@
 # 스택 - 스택 (백준 실버 4)
 # 문제 링크: https://www.acmicpc.net/problem/10828
+
+n = int(input())
+arr = []
+
+for i in range(n):
+    cmd = input().split()
+    
+    if cmd[0] == 'push':
+        arr.append(cmd[1])
+    
+    elif cmd[0] == 'pop':
+        if len(arr) == 0:
+            print(-1)
+        else:
+            print(arr.pop())
+    
+    elif cmd[0] == 'size':
+        print(len(arr))
+
+    elif cmd[0] == 'empty':
+        if len(arr) == 0:
+            print(1)
+        else:
+            print(0)
+
+    elif cmd[0] == 'top':
+        if len(arr) == 0:
+            print(-1)
+        else: 
+            print(arr[-1])
