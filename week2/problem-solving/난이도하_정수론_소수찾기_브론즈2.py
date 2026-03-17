62381bd77d2923c473d67a6774290966fe81827c
백승진 <vromotion@gmail.com>
2026-03-18 00:22:44 +0900

    난이도하_정수론_소수찾기_브론즈2

--- week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
+++ week2/problem-solving/난이도하_정수론_소수찾기_브론즈2.py
@@ -1,2 +1,31 @@
 # 정수론 - 소수 찾기 (백준 브론즈2)
 # 문제 링크: https://www.acmicpc.net/problem/1978
+
+import math
+
+n = int(input())
+nums = list(map(int, input().split()))
+prime = []
+
+for i in range(n):
+    num = nums[i]
+
+    if num < 2:
+        continue
+
+    if num == 2:
+        prime.append(num)
+        continue
+
+    if num % 2 == 0:
+        continue
+
+    limit = int(math.sqrt(num))
+
+    for j in range(3, limit + 1, 2):
+        if num % j == 0:
+            break
+    else:
+        prime.append(num)
+
+print(len(prime))
