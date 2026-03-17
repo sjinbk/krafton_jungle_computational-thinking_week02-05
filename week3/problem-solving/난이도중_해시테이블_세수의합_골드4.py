c850bb1a664f65e12630b5b48864146b6378692e
백승진 <vromotion@gmail.com>
2026-03-17 09:24:22 +0900

    난이도중_해시테이블_세수의합_골드4

--- week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
+++ week3/problem-solving/난이도중_해시테이블_세수의합_골드4.py
@@ -1,2 +1,32 @@
 # 해시 테이블 - 세 수의 합 (백준 골드4)
 # 문제 링크: https://www.acmicpc.net/problem/2295
+
+n = int(input())
+
+nums = []
+for _ in range(n):
+    nums.append(int(input()))
+
+# x+y = k-z 가 존재하면, 이때 최대 k값 출력
+x_plus_y = []
+for i in range(len(nums)):
+    for j in range(len(nums)):
+            x_plus_y.append(nums[i] + nums[j])
+x_plus_y = set(x_plus_y)
+
+
+k_minus_z = []
+for i in range(len(nums)):
+    for j in range(len(nums)):
+            if nums[i] > nums[j]:
+                k_minus_z.append(nums[i] - nums[j])
+k_minus_z = set(k_minus_z)
+
+# nums.reverse() 와 구분
+nums.sort(reverse=True)
+
+for k in nums:
+    for z in nums:
+        if (k - z) in x_plus_y:
+            print(k)
+            exit()
