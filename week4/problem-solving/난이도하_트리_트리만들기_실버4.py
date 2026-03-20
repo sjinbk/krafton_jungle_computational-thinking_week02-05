bc286015f0a67f95837cf3c4ccb9421be3d431ad
백승진 <vromotion@gmail.com>
2026-03-20 20:37:48 +0900

    난이도하_트리_트리만들기_실버4

--- week4/problem-solving/난이도하_트리_트리만들기_실버4.py
+++ week4/problem-solving/난이도하_트리_트리만들기_실버4.py
@@ -1,2 +1,16 @@
 # 트리 - 트리 만들기 (백준 실버4)
 # 문제 링크: https://www.acmicpc.net/problem/14244
+
+n, m = map(int, input().split())
+
+# 초기값
+print("0 1")
+
+# 리프 m개 
+for i in range(2, m+1):
+    print(f"1 {i}")
+
+# 차수 2이상 노드 (n-1)-m개
+if n-1-m > 0:
+    for i in range(0, n-1-m):
+        print(f"{m+i} {m+i+1}")
