9ba3322d431eddbb7570aefaa6ca0533820582b3
백승진 <vromotion@gmail.com>
2026-03-27 11:27:03 +0900

    01_dp_fibonacci

--- week5/basic/01_dp_fibonacci.py
+++ week5/basic/01_dp_fibonacci.py
@@ -84,15 +84,23 @@ def fibonacci_memo(n, memo=None):
         n번째 피보나치 수
     """
     # TODO: memo가 None이면 빈 딕셔너리로 초기화
+    if memo is None:
+        memo = {}
     pass
     
     # TODO: base case 
+    if n <=1:
+        return n
     pass
     
     # TODO: 이미 계산한 값이 memo에 있으면 반환
+    ## 요청한 n번째 피보나치 값(딕셔너리에 n의 key값으로 저장되어 있음)이 value값으로 반환
+    if n in memo:
+        return memo[n]
     pass
     
     # TODO: 재귀 호출하여 계산하고 memo에 저장
+    memo[n] = fibonacci_memo(n-1, memo) + fibonacci_memo(n-2, memo)
     pass
     
     return memo[n]
