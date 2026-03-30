b757b4fdfecbd0a9d3bc9213591bf50a8549ece2
백승진 <vromotion@gmail.com>
2026-03-30 10:29:43 +0900

    난이도하_그리디_잃어버린괄호_실버2

--- week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
+++ week5/problem-solving/난이도하_그리디_잃어버린괄호_실버2.py
@@ -1,2 +1,18 @@
 # 그리디 - 잃어버린 괄호 (백준 실버2)
 # 문제 링크: https://www.acmicpc.net/problem/1541
+
+import sys
+input = sys.stdin.readline
+
+# 마이너스 기준으로 분리
+expr = input().strip()
+parts = expr.split('-')
+
+# 첫 마이너스 이전의 숫자합은 무조건 양수
+total = sum(map(int, parts[0].split('+')))
+
+# 첫 마이너스 이후 숫자들은 무조건 빼기
+for part in parts[1:]:
+    total -= sum(map(int, part.split('+')))
+
+print(total)
